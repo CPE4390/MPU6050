4331a1391ddb685a54e8c309880a41cdaa48a90c
EET4340 <45945733+EET4340@users.noreply.github.com>
2019-01-15 14:10:37 -0700

    Initial commit

--- src/MPU6050.c
+++ src/MPU6050.c
@@ -1,2 +1,26 @@
 #include <xc.h>
 #include "MPU6050.h"
+
+
+int pic18_i2c_enable(void) {
+    return 0;
+}
+
+int pic_18_i2c_disable(void) {
+    return 0;
+}
+
+int pic_18_i2c_write(unsigned char slave_addr, unsigned char reg_addr, unsigned char length, unsigned char const *data) {
+    
+    return 0;
+}
+
+int pic18_i2c_read(unsigned char slave_addr, unsigned char reg_addr, unsigned char length, unsigned char *data) {
+    
+    return 0;
+}
+
+int get_ms(unsigned long *count) {
+    *count = 0;
+    return 0;
+}
--- src/MPU6050.h
+++ src/MPU6050.h
@@ -8,12 +8,28 @@
 #ifndef MPU6050_H
 #define	MPU6050_H
 
+#include <xc.h>
+
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
-
-
+    int pic18_i2c_enable(void);
+    int pic18_i2c_disable(void);
+    int pic18_i2c_write(unsigned char slave_addr,
+                     unsigned char reg_addr,
+                     unsigned char length,
+                     unsigned char const *data);
+    int pic18_i2c_read(unsigned char slave_addr,
+                    unsigned char reg_addr,
+                    unsigned char length,
+                    unsigned char *data);
+    
+#define _XTAL_FREQ      32000000ul
+#define delay_ms        __delay_ms
+    int get_ms(unsigned long *count);
+    
+#define MPU6050
 
 #ifdef	__cplusplus
 }
